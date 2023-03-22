7ac53aac0b80210778105a961dfb013921b5da19
pawelmackiewicz <35434527+pawelmackiewicz@users.noreply.github.com>
2023-03-22 20:58:00 +0100

    added sh call just to see if job will fail the same

--- vars/foo.groovy
+++ vars/foo.groovy
@@ -1,4 +1,4 @@
 def call(Map config = [:]) {
 	loadLinuxScript(scriptName: 'hello-world.sh')
-	./hello_world.sh "${config.name} ${config.date}"
+	sh "./hello_world.sh ${config.name} ${config.date}"
 }
