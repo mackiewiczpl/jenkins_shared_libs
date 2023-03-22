8dfc40f8729dbcbf2b8628a660bf86b0dd512aa6
pawelmackiewicz <35434527+pawelmackiewicz@users.noreply.github.com>
2023-03-22 22:26:38 +0100

    added diag commands

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -2,5 +2,6 @@ def call(Map config = [:]) {
 	def scriptcontent = libraryResource "org/foo/scripts/linux/${config.scriptName}"
 	writeFile file: "${config.scriptName}", text: scriptcontent
 	sh "chmod a+x ./${config.scriptName}"
-	sh "ls ./"
+	sh "pwd"
+	sh "ls -la ./"
 }
