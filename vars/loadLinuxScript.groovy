d5d6bb882211ac5b2205f7b28186c6bf71c7696b
pawelmackiewicz <35434527+pawelmackiewicz@users.noreply.github.com>
2023-03-22 20:44:50 +0100

    changed call from bash to sh - not sure if this will work

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]) {
 	def scriptcontent = libraryResource "org/foo/scripts/linux/${config.scriptName}"
 	writeFile file: "${config.scriptName}", text: scriptcontent
-	bash "chmod a+x ./${config.scriptName}"
+	sh "chmod a+x ./${config.scriptName}"
 }
